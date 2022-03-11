bc12f853bbdd39017aa97a676bac8591b9ec95f9
MD.OLI ULLAH ANTOR <85104011+Antor-094@users.noreply.github.com>
2022-03-11 08:18:06 +0600

    html template added

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,9 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
+Route::get('/',[HomeController::class,'index']);
 Route::get('/home',[HomeController::class,'redirect']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
