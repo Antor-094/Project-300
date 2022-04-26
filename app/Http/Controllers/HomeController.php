cffdae891a23c1750533504d6c012060d0fc7bd7
MD.OLI ULLAH ANTOR <85104011+Antor-094@users.noreply.github.com>
2022-04-26 16:49:32 +0600

    Uploaded  data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Doctor;
+use App\Models\Appointment;
 
 class HomeController extends Controller
 {
@@ -26,7 +27,7 @@ class HomeController extends Controller
 
     }
         public function index()
-        {  
+        {
             if(Auth::id()){
                 return redirect('home');
             }
@@ -35,7 +36,32 @@ class HomeController extends Controller
                return view('user.home',compact('doctor'));
 
             }
-             
+
          }
-         
+
+         public function appointment(Request $request)
+         {
+             $data = new appointment;
+
+             $data->name=$request->name;
+             $data->email=$request->email;
+             $data->date=$request->date;
+             $data->phone=$request->number;
+             $data->message=$request->message;
+             $data->doctor=$request->doctor;
+             $data->status='In progress';
+
+             if(Auth::id())
+             {
+                $data->user_id=Auth::user()->id;
+             }
+             $data->save();
+
+             return redirect()->back()->with('message','Appointment Request Successful.
+              we will contact with you soon');
+
+
+
+         }
+
 }
