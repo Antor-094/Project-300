505b882db8809ff30b51253f7f653dea038ea09e
MD.OLI ULLAH ANTOR <85104011+Antor-094@users.noreply.github.com>
2022-04-27 17:45:40 +0600

    Approve and Cancel appointment by Admin

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -56,13 +56,22 @@
           <li class="nav-item nav-category">
             <span class="nav-link">Navigation</span>
           </li>
-          
+
           <li class="nav-item menu-items">
             <a class="nav-link" href="{{url('add_doctor_view')}}">
               <span class="menu-icon">
                 <i class="mdi mdi-file-document-box"></i>
               </span>
               <span class="menu-title">Add Doctors</span>
+            </a>
+          </li>
+
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="{{url('showappointment')}}">
+              <span class="menu-icon">
+                <i class="mdi mdi-file-document-box"></i>
+              </span>
+              <span class="menu-title">Appointment</span>
             </a>
           </li>
         </ul>
